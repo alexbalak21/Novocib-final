98f8ca7604adadba25326eca8952ea806f27aba4
alexbalak21 <alex.balak@outlook.com>
2024-02-18 13:21:55 +0100

    card changed for final design.

--- app/components/Card.php
+++ app/components/Card.php
@@ -1,18 +1,49 @@
 <?php
 class Card
 {
-  static function get($img_src = "", $card_title = "", $card_content = "")
+  static function get($img_src = "app/static/img/catalogue/analysis.jpg", $card_title = "", $card_content = "")
   {
     return <<<CARD
-        <div class="card my-3" style="width: 18rem;">
-  <img src="$img_src" class="card-img-top" alt="..." style="height: 14rem;">
-  <div class="card-body">
-    <h5 class="card-title">$card_title</h5>
-    <p class="card-text">$card_content</p>
-    <div class="text-center"><a href="#" class="btn btn-primary">Go somewhere</a></div>
-    
-  </div>
-</div> 
+    <div class="box text-center" >
+        <img src="$img_src"/>
+        <a href=""><h5>Analytical services</h5></a>
+        <p>Description of services and products, Description of services and products</p>
+        <div class="text-center my-4"><a href="#" class="btn btn-primary">Go to Service</a></div>
+    </div>
 CARD;
   }
 }
+?>
+
+<style>
+  .box {
+    width: 20rem;
+    box-shadow: 0 0 45px rgba(0, 0, 0, 0.06);
+    box-shadow: 1px 0 1.04rem rgba(21, 21, 21, 0.1);
+    margin-top: 6px;
+    margin-bottom: 60px;
+
+    h5 {
+      margin-top: 20px;
+      margin-bottom: 14px;
+    }
+
+    img {
+      object-fit: cover;
+      height: 10rem !important;
+      width: 100%;
+    }
+  }
+
+  .box a h5 {
+    color: initial;
+  }
+
+  .box a {
+    text-decoration: none;
+  }
+
+  .box a h5:hover {
+    color: var(--novo-blue)
+  }
+</style>
