8f5a84d60358bedb8492c18dad4e9016a9f43814
alexbalak21 <alex.balak@outlook.com>
2024-03-05 19:56:32 +0100

    Anlitycal Services Update

--- app/views/analytical-services.php
+++ app/views/analytical-services.php
@@ -9,14 +9,14 @@ $path_way = ["home", "Analytical Services"];
 
 addContent(Banner::gen(img_url: "app/static/img/hplc3.jpg"));
 $content_title = UnderlinedTitle::get("Analytical Services for full nucleotides spectra:", "novoblue", "center");
-
+$article_title = $content_title = UnderlinedTitle::get("Nucleotides: numerous biological roles", "novoblue", "center");
 $page_content = <<<Services
 <div class="container mt-5">
 $content_title
 
 <img class="w-100 my-4" src="/app//static/img/full-nucleotides-spectra.jpg" alt="Full nucleotides spectra presentation" />
 
-<div>
+<article>
 <h6>Nucleotides are present in cells in different forms:</h6>
 <ul>
 <li>Apolar heterocyclic bases (purines and pyrimidines) and nucleosides;</li>
@@ -25,49 +25,24 @@ $content_title
 </ul>
 
 <p>NOVOCIB offers a range of analytical services from characterisation of free 5' nucleotides to full spectrum of nucleotides  (RNA, DNA, NMP, nucleosides and bases)  in feed/ food products and ingredients using  ion-pair high-performance liquid chromatography with diode array detection (Agilent 1200 HPLC-UV). </p>
+<div class="text-center">
+<img class="mt-5 mb-4" style="border: 1px solid silver;" src="app/static/img/column-evolution-time.jpg" alt="">
 </div>
-
-
-<p>Nucleosides, nucleotides and nucleic acids are essential group of molecules playing several important biological functions including maintaining and transferring genetic information, providing energy, being a part of coenzymes or intracellular signal transduction in cells.</p>
-<article>
-<h6>Nucleotide can be present in cells or in food/feed in different forms:</h6>
-<ul>
-<li>Apolar heterocylic bases (purines and pyrimidines) and nucleosides;</li>
-<li>Polar nucleotides mono-, di- and triphosphates;</li>
-<li>Negatively charged nucleic acids RNA and DNA.</li>
-</ul>
-</article>
-<p>Since nucleotides can be synthesized de novo and recycled through salvage pathways, they are considered as semi-essential nutrients.  However, rapidly growing tissues or rapidly dividing immune cells during infection have higher requirements cells for nucleotides that cannot be met only through de novo synthesis. In these cases, nucleotides become "essential" nutriments that can be provided with aliments and assimilated through shorter salvage pathway (2 steps) compared to de novo synthesis (9 steps for purines). <br> <br></p>
-
-<article>
-<h6>It has been shown that the supplementation of feed with dietary nucleotides : <br></h6>
-<ul style="list-style-type: '- ';">
-<li>Boosts immune system, enhances immunity and resistance to bacterial infections; <br></li>
-<li>Accelerates intestinal recovery after diarrhoea or food deprivation; <br></li>
-<li>Improves growth rate.</li>
-</ul>
-
 </article>
 
-<article>
-    <h6>Sources of dietary nucleotides :</h6>
-    <ul>
-        <li>Since animal muscle is naturally rich in ATP, meat (pork, beef, chicken), fish and shrimps are excellent sources of purine nucleotides;</li>
-        <li>Baker yeasts are naturally rich in RNA and yeast extracts are excellent sources of both purine and pyrimidine nucleotides.</li>
-    </ul>
-</article>
-
-<article>
-<h6>"Tasty" nucleotides:</h6>
-<p>5'-nucleotides IMP and GMP (also known as Inosinate and Guanylate) are strong flavour enhancers (E626-E633) involved in "umami" taste. Since yeasts are rich in ribonucleic acid (RNA), a natural source of 5' nucleotides (GMP, AMP, CMP and UMP), yeast extracts enriched in 5' nucleotides IMP and GMP are currently used as taste enhancers</p>
-</article>
 <article class="my-5">
 <div class="d-flex justify-content-center">
-<table class="table mx-2 product text-center">
+<table class="table w-100 product">
     <thead>
         <tr>
             <th>
-                SERVICE NAME
+                #REF
+            </th>
+            <th class="w-75 text-center">
+                PRODUCT NAME
+            </th>
+            <th>
+                PRICE
             </th>
             <th>
             </th>
@@ -75,30 +50,63 @@ $content_title
     </thead>
     <tbody>
         <tr>
-            <td><a href="">HPLC analysis of bases, nucleosides, NMP and nucleic acids in feed ingredients or products (request a quote)</a></td>
-            <td><a class="btn btn-primary" href="">Inquiry</a></td>
+            <td>#S1200-03</td>
+            <td class="text-center"><h6>Dietary Nucleotides Analysis:</h6><p>HPLC-UV analysis for full spectra of soluble dietary nucleotides (bases, nucleosides and NMP, unhydrolyzed nucleic acid) present in feed or ingredients</p></td>
+            <td class="text-center">€ 350.00<br>per<br>Sample</td>
+            <td><a class="btn btn-primary" href="/inquiry?product=Yeast Free Bases, Nucleosides, Nucleotides and RNA & Bases">Inquiry</a></td>
         </tr>
         <tr>
-            <td><a href="">HPLC analysis nucleic acids (RNA and DNA) quantification (request a quote);</a></td>
-            <td><a class="btn btn-primary" href="">Inquiry</a></td>
+            <td>#S1200-04</td>
+            <td class="text-center"><h6>Celullar Nucleotides Analysis:</h6><p>HPLC-UV analysis for full spectra of cellular nucleotides (bases, nucleosides, NMP, NDP and NTP) in cell extracts.</p></td>
+            <td class="text-center">€ 350.00<br>per<br>Sample</td>
+            <td><a class="btn btn-primary" href="/inquiry?product=Yeast Free Bases, Nucleosides, Nucleotides and RNA & Bases">Inquiry</a></td>
         </tr>
         <tr>
-            <td><a href="">Purine analysis (request a quote)</a></td>
-            <td><a class="btn btn-primary" href="">Inquiry</a></td>
+            <td>#S1200-05</td>
+            <td class="text-center">Nucleic acid RNA and DNA  quantification by HPLC-UV analysis</td>
+            <td class="text-center">€ 300.00<br>per<br>Sample</td>
+            <td><a class="btn btn-primary" href="/inquiry?product=Yeast Free Bases, Nucleosides, Nucleotides and RNA & Bases">Inquiry</a></td>
         </tr>
         <tr>
-            <td><a href="">HPLC analysis of whole spectra of cellular nucleotides: <br>
-            (deoxy-) ribonucleotides mono-, di-, and triphosphate present in biological samples (cell extracts) (request a quote);</a></td>
-            <td><a class="btn btn-primary mt-3" href="">Inquiry</a></td>
-        </tr>   
+            <td>#S1200-06</td>
+            <td class="text-center">Nucleic acid RNA and DNA  quantification by HPLC-UV analysis</td>
+            <td class="text-center">€ 300.00<br>per<br>Sample</td>
+            <td><a class="btn btn-primary" href="/inquiry?product=Yeast Free Bases, Nucleosides, Nucleotides and RNA & Bases">Inquiry</a></td>
+        </tr>
     </tbody>
 </table>
+</div>
+</article>
+
+
+<article>
+$article_title
+<p>Nucleotides participate in nearly all biochemical processes important for cell development and growth:</p>
+<div class="text-center">
+<img class="mt-5 mb-4 w-25" style="border: 1px solid silver;" src="app/static/img/dna.jpg" alt="">
+</div>
+<ol>
+<li>dNTPs are  building blocks of DNA, particularly important for actively proliferating cells of the immune system, intestinal epithelium and unicellular microorganisms.</li>
+<div class="text-center">
+<img class="mt-5 mb-4" style="border: 1px solid silver;" src="app/static/img/chain.jpg" alt="">
+</div>
+<li> NTPs are building blocks of tRNA, rRNA and mRNA involved in protein synthesis and cell growth.</li>
+<div class="text-center">
+<img class="mt-5 mb-4" style="border: 1px solid silver;" src="app/static/img/muscles.jpg" alt="">
+</div>
+<li>ATP is an universal energy currency in all biological systems particularly abundant in muscles.</li>
+<li>Adenine nucleotides are components of three major coenzymes, NAD+, FAD, and CoA involved in numerous biosynthesis pathways;</li>
+<li>5. UDP-activated glucose is required for glycogen synthesis in liver and skeletal muscles;</li>
+<li>CDP-activated choline is involved in the biosynthesis of phospholipids particularly in in the neuronal membranes;</li>
+<li>GMP and IMP nucleotides are flavor compounds that drastically enhance the umami taste of glutamate and improve the palatability of the food with reduced salt.</li>
+</ol>
+
 </article>
 
 
 <div class="row">
 <img class="mx-4" style="border: 1px solid silver; width: 40%;" src="app/static/img/hplc-column.jpg" alt="">
-<img class="mx-4" style="border: 1px solid silver; width: 40%;" src="app/static/img/column-evolution-time.jpg" alt="">
+
 </div>
 </div>
 Services;
