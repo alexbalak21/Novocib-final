79799b2890be3038e07568154794b1a7e1c037ff
alexbalak21 <alex.balak@outlook.com>
2024-03-06 23:17:51 +0100

    update analytical services

--- app/views/analytical-services.php
+++ app/views/analytical-services.php
@@ -10,14 +10,14 @@ $path_way = ["home", "Analytical Services"];
 addContent(Banner::gen(img_url: "app/static/img/hplc9.jpg"));
 
 $article_title = $content_title = UnderlinedTitle::get("Nucleotides: 'conditionally essential' nutrients", "novoblue", "center");
-$content_title = UnderlinedTitle::get("Full nucleotides spectra by HPLC", "novoblue", "right");
+$content_title = UnderlinedTitle::get("Full nucleotides spectra by HPLC", "novoblue", "center");
 $nuclotide_supplemetantion = UnderlinedTitle::get("Nucleotide supplementation: 'conditionally essential' nutrients", "novoblue", "center");
 $page_content = <<<Services
 <div class="container mt-5">
 $content_title
 
 <article class="row">
-<h5 class="text-center mb-2">Nucleotides are present in cells in different forms:</h5>
+<h5 class="text-center my-3">Nucleotides are present in cells in different forms:</h5>
 <div class="col-lg-6 text-end">
 <img class="h-50 mb-1"  style="width: 488px; border: 1px solid silver;" src="/app/static/img/nucleotides-1.jpg" alt="">
 <img class="h-50" style="width: 488px; border: 1px solid silver;" src="/app/static/img/nucleotides-2.jpg" alt="">
@@ -42,13 +42,6 @@ Negatively charged free ribo- and deoxyribonucleotides mono-, di- and triphospha
 
 <div class="col-lg-5 d-flex align-items-center">
 <div class="">
-<p>Nucleotides are present in cells in different forms:
-    <ul>
-    <li>Apolar heterocyclic bases (purines and pyrimidines) and nucleosides; </li>
-    <li>Polar nucleotides mono-, di- and triphosphates;</li>
-    <li>Negatively charged polymeric nucleic acids RNA and DNA.</li>
-    </ul>
-</p>
 <p>NOVOCIB offers a range of analytical services from characterisation of free 5' nucleotides to full spectrum of nucleotides  (RNA, DNA, NMP, nucleosides and bases)  in feed/ food products and ingredients using  ion-pair high-performance liquid chromatography with diode array detection (Agilent 1200 HPLC-UV).</p>
 </div>
 </div>
@@ -68,10 +61,10 @@ Negatively charged free ribo- and deoxyribonucleotides mono-, di- and triphospha
             <th>
                 #REF
             </th>
-            <th class="w-75 text-center">
+            <th class="w-75 text-center" style="width: 800px !important">
                 PRODUCT NAME
             </th>
-            <th class="text-center" style="width: 400px !important">
+            <th class="text-center">
                 PRICE
             </th>
             <th>
@@ -82,7 +75,7 @@ Negatively charged free ribo- and deoxyribonucleotides mono-, di- and triphospha
         <tr>
             <td>#S1200-03</td>
             <td class="text-center"><h6>Dietary Nucleotides Analysis:</h6><p>HPLC-UV analysis for full spectra of soluble dietary nucleotides (bases, nucleosides and NMP, unhydrolyzed nucleic acid) present in feed or ingredients</p></td>
-            <td class="text-center">€ 350.00<br>per<br>Sample</td>
+            <td class="text-center">€ 350.00 per Sample</td>
             <td><a class="btn btn-primary" href="/inquiry?product=Yeast Free Bases, Nucleosides, Nucleotides and RNA & Bases">Inquiry</a></td>
         </tr>
         <tr>
@@ -93,14 +86,14 @@ Negatively charged free ribo- and deoxyribonucleotides mono-, di- and triphospha
         </tr>
         <tr>
             <td>#S1200-05</td>
-            <td class="text-center">Nucleic acid RNA and DNA  quantification by HPLC-UV analysis</td>
-            <td class="text-center">€ 300.00<br>per<br>Sample</td>
+            <td class="text-center">Nucleic acid RNA and DNA quantification by HPLC-UV analysis <br> &nbsp;</td>
+            <td class="text-center">€ 300.00 per Sample</td>
             <td><a class="btn btn-primary" href="/inquiry?product=Yeast Free Bases, Nucleosides, Nucleotides and RNA & Bases">Inquiry</a></td>
         </tr>
         <tr>
             <td>#S1200-06</td>
-            <td class="text-center">Nucleic acid RNA and DNA  quantification by HPLC-UV analysis</td>
-            <td class="text-center">€ 300.00<br>per<br>Sample</td>
+            <td class="text-center">HPLC-UV analysis for purines <br> &nbsp;</td>
+            <td class="text-center">€ 300.00 per Sample</td>
             <td><a class="btn btn-primary" href="/inquiry?product=Yeast Free Bases, Nucleosides, Nucleotides and RNA & Bases">Inquiry</a></td>
         </tr>
     </tbody>
