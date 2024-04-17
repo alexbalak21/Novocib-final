2ef118342af062563dbf4267474ff7c35b681203
alexbalak21 <alex.balak@outlook.com>
2024-04-17 17:47:53 +0200

    share page done.

--- app/internal/share/index.php
+++ app/internal/share/index.php
@@ -9,14 +9,46 @@
 </head>
 
 <body>
-    <main>
-        <form action="/internal/share">
-            <input type="password">
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        echo <<<FORM
+        <main>
+        <form action="/internal/share" method="POST">
+            <input type="password" name="password">
             <div class="center">
                 <button>Submit</button>
             </div>
         </form>
     </main>
+    FORM;
+    } elseif (isset($_POST['password']) && $_POST['password'] === "root123") {
+        echo <<<DOWNLOAD
+        <main>
+        <div class="center">
+            <a href="/app/internal/files/Black_Vultures.zip">Black Vultures</a>
+        </div>
+    </main>
+    <form action="/internal/share">
+    <div class="center"><button>DISOCONNECT</button></div>
+    </form>
+
+    DOWNLOAD;
+    } else {
+        echo <<<FORM
+        <main>
+        <form action="/internal/share" method="POST">
+        <p class="center red">Worng password</p>
+            <input type="password" name="password">
+            <div class="center">
+                <button>Submit</button>
+            </div>
+        </form>
+    </main>
+    FORM;
+    }
+
+    ?>
+
 </body>
 
 </html>
