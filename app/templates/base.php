37573ee7be7de0c814a69f13738a83ed4660a341
alexblack24 <alex.black@mail.com>
2024-05-03 11:00:02 +0200

    added to base template if fontawasome is local funcion.

--- app/templates/base.php
+++ app/templates/base.php
@@ -12,6 +12,16 @@ if (!isset($metas)) {
     $metas = "";
     echo "";
 }
+$fontAwasome_source = "https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.2-web/css/all.min.css";
+
+function check_fontAwasome_local()
+{
+    global $fontAwasome_source;
+    if (file_exists("app/FA6/css/all.min.css")) {
+        $fontAwasome_source = "/app/FA6/css/all.min.css";
+    }
+}
+check_fontAwasome_local();
 
 ?>
 <!DOCTYPE html>
@@ -25,9 +35,7 @@ if (!isset($metas)) {
     <!-- FONTAWASOME -->
     <!-- <script src="https://kit.fontawesome.com/6502c70df0.js" crossorigin="anonymous"></script> -->
     <!-- LOCAL -->
-    <link href="/app/FA6/css/all.min.css" rel="stylesheet" />
-    <!-- CDN -->
-    <!-- <link href="https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css" rel="stylesheet"> -->
+    <link href="<?= $fontAwasome_source ?>" rel="stylesheet" />
     <!-- BOOTSTRAP  -->
     <link rel="stylesheet" href="/app/css/bootstrap.min.css">
     <script src="/app/js/bootstrap.bundle.min.js" defer></script>
