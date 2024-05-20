d042cfcbb3fd7f2aa0c46a19c78b8950b8c80bac
alexblack24 <alex.black@mail.com>
2024-05-20 18:45:52 +0200

    update

--- app/internal/share/index.php
+++ app/internal/share/index.php
@@ -7,16 +7,19 @@
     <link rel="stylesheet" href="/app/internal/share/style.css">
     <link rel="icon" type="image/x-icon" href="./share.ico" />
     <script src="https://kit.fontawesome.com/6502c70df0.js" crossorigin="anonymous"></script>
-    <title>Sahre</title>
+    <title>Share</title>
 </head>
 
 <body>
+
     <?php
+    $domain = "https://www.novocib.com/";
+    $files = "/app/internal/share/files";
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
         echo <<<FORM
         <main>
         <form action="/internal/share" method="POST">
-            <input type="password" name="password">
+            <input type="password" autofocus name="password">
             <div class="center">
                 <button>Submit</button>
             </div>
@@ -27,20 +30,7 @@
         echo <<<DOWNLOAD
         <main>
         <div class="center">
-            <a href="/app/internal/files/Halestorm.zip">Halestorm.zip <i class="fa-solid fa-music"></i> <i class="fa-solid fa-cloud-arrow-down"></i></a>
-            <br>
-            <br>
-            <a href="/app/internal/files/ACDC - BEST OF.zip">ACDC - BEST OF <i class="fa-solid fa-music"></i> <i class="fa-solid fa-cloud-arrow-down"></i></a>
-            <br>
-            <br>
-            <a href="/app/internal/files/Dorothy.zip">Dorothy<i class="fa-solid fa-music"></i> <i class="fa-solid fa-cloud-arrow-down"></i></a>
-            <br>
-            <br>
-            <a href="/app/internal/files/Dune1.mp4" download>Dune 1 <i class="fa-solid fa-film"></i> <i class="fa-solid fa-cloud-arrow-down"></i></a>
-            <br>
-            <br>
-            <a href="/app/internal/files/Beekeeper.mp4" download>Beekeeper<i class="fa-solid fa-film"></i> <i class="fa-solid fa-cloud-arrow-down"></i></a>
-       
+            <a href="/app/internal/share/files/Bullet_Train.mkv">Bullet Train<i class="fa-solid fa-film"></i> <i class="fa-solid fa-cloud-arrow-down"></i></a>
     </main>
     <form action="/internal/share">
     <div class="center"><button>DISCONNECT</button></div>
@@ -51,7 +41,7 @@
         echo <<<FORM
         <main>
         <form action="/internal/share" method="POST">
-        <p class="center red">Worng password</p>
+        <p class="center red">Wrong password</p>
             <input type="password" name="password">
             <div class="center">
                 <button>Submit</button>
