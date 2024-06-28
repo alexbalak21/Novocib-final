61a0144789a3704db84443b437cbd419c635e5c8
alexblack24 <alex.black@mail.com>
2024-06-28 10:04:57 +0200

    robots update

---
+++ app/views/base copy 2.php
@@ -0,0 +1,21 @@
+<?php
+$title = "Base";
+require_once "app/templates/base.php";
+
+$novoblue = "#4167b1";
+
+
+
+
+addContent(Banner::gen());
+$content_title = UnderlinedTitle::get("Base Template Page", "novoblue", "right");
+
+$page_content = <<<CONTENT
+<div class="container mt-5">
+$content_title
+<p>NOVOCIB produces active nucleoside kinases and other nucleotide metabolism enzymes. This portfolio of enzymes represents a unique range of powerful tools for Drug Discovery and research in the field of nucleoside and nucleotide metabolism.</p>
+</div>
+CONTENT;
+
+addContent($page_content);
+render();
