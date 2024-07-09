91dc2dceaa44a63d5115eb3090ca31d750c3c144
alexblack24 <alex.black@mail.com>
2024-07-09 17:51:20 +0200

    nav update

--- app/components/Nav.php
+++ app/components/Nav.php
@@ -62,7 +62,7 @@ class Nav
                         <ul>
                             <li><a href="/convenient-assay-kits/human-recombinant-impdh">Human IMPDH type II</a></li>
                             <li><a href="/convenient-assay-kits/bacterial-recombinant-impdh">Bacterial IMPDH</a></li>
-                            <!-- <li><a href="/active-purified-enzymes/impdh-recombinant-enzyme">IMPDH Recombinant Enzyme</a></li> -->
+                            <!-- <li><a href="/active-purified-enzymes/impdh-recombinant-enzyme">IMPDH Recombinant Enzyme</a></li> -->f
                             <li>
                                 <a href="/active-purified-enzymes/human-recombinant-hgprt">Human Recombinant HGPRT</a>
                             </li>
