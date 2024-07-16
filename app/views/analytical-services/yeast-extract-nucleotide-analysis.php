3091e6d1c76eb13dc6ee32a90c5270e5b4fcc949
alexblack24 <alex.black@mail.com>
2024-07-16 11:10:04 +0200

    update yeast page

--- app/views/analytical-services/yeast-extract-nucleotide-analysis.php
+++ app/views/analytical-services/yeast-extract-nucleotide-analysis.php
@@ -1,5 +1,5 @@
 <?php
-$title = "Yeast Extract Nucleotide Analysis";
+$title = "Yeast Extracts Nucleotides and Nucleic Acids";
 require_once "app/templates/base.php";
 
 $novoblue = "#4167b1";
@@ -9,33 +9,55 @@ $content_title = UnderlinedTitle::get("Yeast Extracts Enriched in Tasty Nucleoti
 
 
 ob_start(); ?>
-<div class="container mt-5">
-    $content_title
-    <p>5'-nucleotides IMP and GMP (also known as Inosinate and Guanylate) are strong flavour enhancers (E626-E633) involved in "umami" taste. Since yeasts are rich in ribonucleic acid (RNA), a natural source of 5' nucleotides (GMP, AMP, CMP and UMP), yeast extracts enriched in 5' nucleotides IMP and GMP are currently used as taste enhancers.</p>
-    <div class="text-center mb-5">
-        <img onclick="enlargeImg(this)" class="zoom" src="/app/static/img/column-evolution-time.jpg" alt="">
-        <p class="text-muted" style="font-size:18px !important;"><i class="fa-solid fa-hand-pointer"></i> Click to enlarge</p>
+<main class="container mt-5">
+    <?= $content_title ?>
+    <div class="row my-5">
+        <div class="col-lg-4">
+            <img src="/app/static/img/yeast.jpg" alt="yeast picture" title="yeast" class="w-100">
+        </div>
+        <div class="col-lg-8">
+            <p> Yeast extract is a versatile ingredient widely used as nutritious and flavor complement for food and feed formulation. It's derived from yeast cells and contains essential nutrients such as high-quality protein, B vitamins, amino acids, minerals, nucleotides and nucleic acid RNA.</p>
+            <p>Yeast cells are naturally abundant in RNA, constituting approximately 6–12% of their dry weight. Such high RNA content makes yeast a valuable raw material for the production of 5'-nucleotides and in particular compounds like inosinate (IMP) and guanylate (GMP), natural flavor enhancers. When combined with other flavor compounds (glutamate or salt), IMP and GMP enhance overall taste perception and reduce the need for salt in food.</p>
+        </div>
+    </div>
+    <div class="row mb-5">
+        <div class="col-lg-6 d-flex align-items-center">
+            <div>
+                <h4 class="novo-blue mb-2">Yeast tasty 5'-nucleotides:</h4>
+                <p>Typically, using microbial nucleases yeast RNA is hydrolyzed into four 5'-nucleotides (5'AMP, 5'GMP, 5'UMP and 5'CMP). To produce tasty yeast extracts rich in inosinate (5'IMP) and guanylate (5'GMP), yeast extract area treated with microbial AMP-deaminase that converts 5'AMP to 5'IMP.</p>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <img class="w-100" src="/app/static/img/yeast-rna.jpg" alt="yeast rna schema" title="Yeast RNA Schema">
+        </div>
+    </div>
+    <div class="pt-5 mb-5">
+        <h5><b>However, several factors can lead to low 5'-nucleotide concentrations in yeast extract and impact its taste properties:</b></h5>
+        <ol>
+            <li>
+                <p><b>Actives phosphatases and 5' nucleases:</b> If phosphatases and 5'nucleases enzymes in yeast extracts are not deactivated prior to RNA hydrolysis, they will remove phosphate groups from nucleotides, resulting in nucleosides lacking flavor-enhancing properties.
+                    Yeast phosphatases can be inactivated through heating or by using acidic or alkaline conditions during autolysis step. However, some inactivation methods may result in the formation of non-tasting 2’,3’-nucleotides, while the content of flavorful 5’-nucleotides would remain low.
+                </p>
+            </li>
+            <li>
+                <p><b>Incomplete RNA Hydrolysis:</b> When nucleases are used under suboptimal conditions, they may not fully hydrolyze RNA molecules. This incomplete degradation could lead to higher RNA levels and consequently lower 5’-nucleotide concentrations.
+                    Certain proteins can bind to RNA molecules, forming protective complexes. These protein-RNA interactions shield the RNA from nucleases, preventing their degradation. As a result, the concentration of 5’-nucleotides may be affected.
+                </p>
+            </li>
+        </ol>
+    </div>
+    <div class="mb-4">
+        <h5><b>RNA co-purified with yeast protein:</b></h5>
+        <p class="mb-3">Yeast proteins contain all essential amino acids, are environmentally friendly and offer high bioavailability, making them a promising alternative to animal protein for food applications. However, yeast proteins can be co-purified with significant amounts of polymeric RNA especially when purified using microfiltration. Since excessive RNA intake can lead to the production of uric acid, it is recommended that adults do not exceed a daily RNA ingestion of 2 grams.</p>
+        <span class="lead">NOVOCIB provides complete analytical HPLC service to characterize full spectra of nucleotides and nucleic acids present in yeast extracts and yeast proteins - from heterocyclic bases, nucleosides, nucleotides to nucleic acids (RNA and DNA).</span>
+        <div class="my-3 text-center"><span class="lead">To know more : </span> <a class="lead" href="/nucleotide-analysis-service">Nucleotide Analysis Service</a></div>
+    </div>
+    <div class="text-center mb-4">
+        <a class="lead" href="/contact-us">Contact us</a><span class="lead">, if you have any further questions or need additional information</span>
     </div>
 
-    <p>5'-Nucleotidases and phosphatases are ubiquitous hydrolytic enzymes that catalyze the hydrolysis of nucleotides into a phosphate and nucleosides deprived of flavour-enhancing properties. The nucleosides are further hydrolyzed to purine and pyrimidine bases. Purine base hypoxanthine has a strongly bitter taste. Nucleotides/nucleosides/bases content in yeast extract vary as a function of growth state and the method of preparation.</p>
-    <div class="text-center">
-        <img class="w-50 mb-5" src="/app/static/img/yeast-rna.jpg" alt="">
-    </div>
 
-    <p class="mb-4"><strong>Quality Control of Yeast Extract Nucleotides: <span class="novocib">NOVO<span>CIB</span></span> </strong> offers a range of analytical services from characterisation of free 5' nucleotides to full spectrum of nucleotides in yeast extracts by ion-pair high-performance liquid chromatography with diode array detection. This service allows to control the content of flavor nucleotides yeast extract samples.</p>
-
-    <h5>Analytical system:</h5>
-    <p>Agilent 1100 series liquid chromatograph fitted with binary pump, vacuum degassers, well-plate autosampler, thermcostatted column compartment and multiple wavelenght and diode array detector. <br>
-        Run and data acquisision are controlled by Agilent Chem Station software. Zorbax Extend-C18 4.6x150mm, 3.5μm particle size and guard column (Agilent). <br>
-        Calibrations are performed with standards prepared in mobile phase and with standards mixed with cell extracts, which are run immediately before and after every series of samples. <br>
-        Peak assignment of different bases, ribonucleosides and ribonucleoside monophosphatesis is done by comparing both retention times and characteristics of UV absorption spectra (254/280 ratio) with those of standards. The area of individual peaks was measured using ChemStation software (Agilent).</p>
-
-    <div class="text-center">
-        <img onclick="enlargeImg(this)" class="zoom" src="/app/static/img/yeast-amp-deamination.png" alt="yeast-amp-deamination">
-        <p class="text-muted" style="font-size:18px !important;"><i class="fa-solid fa-hand-pointer"></i> Click to enlarge</p>
-    </div>
-</div>
-
+</main>
 <?php $page_content = ob_get_clean();
 
 
