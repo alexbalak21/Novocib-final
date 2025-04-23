33b326b36bf0e1d40f18d6ec58497e06bcb2a090
Alex Balak <alex.balak@outlook.com>
2025-04-23 11:20:14 +0200

    update with AI

--- app/views/active-purified-enzymes/cn-ii.php
+++ app/views/active-purified-enzymes/cn-ii.php
@@ -2,7 +2,7 @@
 $title = "Human cytosolic 5'-nucleotidase II";
 
 ob_start(); ?>
-<meta name="keywords" content="buy 5'-nucleotidase, recombinant enzyme, cytosolic 5'-nucleotidase, E.C. 3.1.3.5, phosphotransferase, high Km 5'-nucleotidase, HKM-NT, cytosolic purine 5'-nucleotidase, purine 5'-NT, IMP/GMP-specific 5'-nucleotidase, ribavirin phosphorylation">
+<meta name="keywords" content="buy 5'-nucleotidase, recombinant enzyme, cytosolic 5'-nucleotidase, E.C. 3.1.3.5, phosphotransferase, high Km 5'-nucleotidase, HKM-NT, cytosolic purine 5'-nucleotidase, purine 5'-NT, IMP/GMP-specific 5'-nucleotidase, ribavirin phosphorylation, research enzyme">
 <meta name="description" content="Novocib's cloned human cytosolic 5'-nucleotidase II is an active, purified enzyme characterized by its capacity to dephosphorylate GMP and IMP, as well as phosphorylate natural nucleosides like inosine and the antiviral nucleoside analog ribavirin.">
 <?php $metas = ob_get_clean();
 
@@ -29,9 +29,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/app/templates/new_base.php";
                     <td class="text-center"><strong>50 mUnits</strong></td>
                     <td class="price text-center">€ 305.00</td>
                     <td class="text-end pe-3">
-                        <a class="btn btn-primary" href="/inquiry?ref=Nov6-50&amp;product=cN-II 50mU">
-                            Inquiry
-                        </a>
+                        <a class="btn btn-primary" href="/inquiry?ref=Nov6-50&amp;product=cN-II 50mU">Inquiry</a>
                     </td>
                 </tr>
                 <tr>
@@ -39,18 +37,13 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/app/templates/new_base.php";
                     <td class="text-center"><strong>150 mUnits</strong></td>
                     <td class="price text-center">€ 650.00</td>
                     <td class="text-end pe-3">
-                        <a class="btn btn-primary" href="/inquiry?ref=Nov6-100&amp;product=cN-II 100mU">
-                            Inquiry
-                        </a>
+                        <a class="btn btn-primary" href="/inquiry?ref=Nov6-100&amp;product=cN-II 100mU">Inquiry</a>
                     </td>
                 </tr>
             </tbody>
         </table>
 
-        <p class="text-muted text-center">
-            <em>Pricing updated on July 23, 2024.</em>
-            <br>
-        </p>
+        <p class="text-muted text-center"><em>Pricing updated on July 23, 2024.</em></p>
         <p class="text-center">
             <strong>
                 Kit is provided in stable lyophilized form and
@@ -58,36 +51,52 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/app/templates/new_base.php";
             </strong>
         </p>
         <p class="my-4 text-center">
-            <strong>You can ask us for a quotation <a href="mailto:contact@novocib.com">here</a> or write at <a href="mailto:contact@novocib.com">contact@novocib.com</a></strong>
+            <strong>Need more details or a custom quote? <a href="mailto:contact@novocib.com">Contact us</a> today at <a href="mailto:contact@novocib.com">contact@novocib.com</a>.</strong>
         </p>
     </div>
-    <p class="mt-5"><span class="novocib">NOVOCIB</span>'S human cytosolic IMP/GMP specific 5'-nucleotidase/phosphotransferase II (cN-II) is a pure and active protein of 65kDa cloned by RT-PCR amplification of mRNA extracted from human hepatoma cells and expressed in E.coli. The sequence of the cloned NT5C2 gene (GenBank accession number P49902) was confirmed by DNA sequencing (100% identity). <br>
-        Cytosolic 5'-nucleotidase II is one of the seven known mammalian nucleotidases(1) that specifically catalyzes the dephosphorylation of 6-hydroxypurine nucleoside 5'-monophosphates (IMP, dIMP, GMP, dGMP) and regulates cellular pool of IMP and GMP(2,3). The enzyme also acts as a phosphotransferase catalyzing the transfer of a phosphate from nucleoside monophosphate to a nucleoside acceptor ' preferentially inosine and deoxyinosine. Unlike the other 5'-nucleotidases, cN-II is allosterically regulated by adenine/guanine nucleotides and 2,3-biphosphoglycerate(4). <br>
-        In addition, cytosolic 5'-nucleotidase II phosphorylates anti-viral and anti-tumour nucleoside analogues such as 2'3'-dideoxyinosine, carbovir(5), acyclovir(6) and ribavirin(7).
+
+    <p class="mt-5">
+        <span class="novocib">NOVOCIB</span>'S human cytosolic IMP/GMP-specific 5'-nucleotidase/phosphotransferase II (cN-II) is a pure and active protein of 65kDa cloned by RT-PCR amplification of mRNA extracted from human hepatoma cells and expressed in <em>E.coli</em>. The sequence of the cloned NT5C2 gene (GenBank accession number P49902) was confirmed by DNA sequencing (100% identity). <br>
+        Cytosolic 5'-nucleotidase II specifically catalyzes the dephosphorylation of 6-hydroxypurine nucleoside 5'-monophosphates (IMP, dIMP, GMP, dGMP) and regulates cellular pools of IMP and GMP. The enzyme also acts as a phosphotransferase catalyzing phosphate transfer from nucleoside monophosphates to nucleoside acceptors, preferentially inosine and deoxyinosine. Unlike other 5'-nucleotidases, cN-II is allosterically regulated by adenine/guanine nucleotides and 2,3-biphosphoglycerate. <br>
+        In addition, cN-II phosphorylates antiviral and antitumor nucleoside analogues such as 2'3'-dideoxyinosine, carbovir, acyclovir, and ribavirin.
     </p>
-    <p><b>Synonyms:</b> cytosolic 5'-nucleotidase/phosphotransferase, High Km 5'-nucleotidase (hkm-NT), cytosolic purine 5µ-nucleotidase (purine 5'-NT), IMP/GMP-specific 5µ-nucleotidase</p>
-    <p class="my-5 mb-5 text-end">Download <a target="_blank" href="/app/documents/Novocib E-Nov6.pdf"> <strong>Human cytosolic 5'-nucleotidase II <i class="fa-regular fa-file-pdf"></i></strong></a><strong> </strong>(User manual)</p>
+
+    <p><b>Synonyms:</b> cytosolic 5'-nucleotidase/phosphotransferase, High Km 5'-nucleotidase (HKM-NT), cytosolic purine 5'-nucleotidase (purine 5'-NT), IMP/GMP-specific 5'-nucleotidase.</p>
+    <p class="my-5 mb-5 text-end">
+        Download <a target="_blank" href="/app/documents/Novocib E-Nov6.pdf">
+            <strong>Human cytosolic 5'-nucleotidase II <i class="fa-regular fa-file-pdf"></i></strong>
+        </a> (User manual)
+    </p>
+
     <div class="row mt-5">
         <div class="col-lg-6">
-            <p><b>Assay condition:</b> Phosphotransferase activity of human recombinant cN-II was followed at 37°C, in 100mM Tris-HCl pH 9; 100mM KCl, 20mM MgCl2, 0.5mM KH2PO4, 5mM DTT, 5mM NAD, 50mU/mL IMPDH (NovoCIB). 5mM GMP was used as phosphate donor and 5mM inosine as - phosphate acceptor. The formation of IMP from inosine was followed at 340nm through coupling to recombinant IMP-dehydrogenase (IMPDH, NovoCIB). In the presence of NAD, IMPDH catalyzes irreversible conversion of IMP to XMP with concomitant reduction of NAD to NADH2 (specific absorbance at 340nm).</p>
-            <div class="col-lg-8 mx-auto">
-                <img class="w-100" src="/app/static/img/cNII-phosphotransferase.jpg" alt="Illustration of phosphotransferase activity of cN-II enzyme">
-            </div>
+            <p><b>Assay condition:</b></p>
+            <ul>
+                <li>Temperature: 37°C</li>
+                <li>Buffer: 100mM Tris-HCl pH 9</li>
+                <li>Reagents: 100mM KCl, 20mM MgCl2, 0.5mM KH2PO4, 5mM DTT, etc.</li>
+                <li>Phosphate donor: 5mM GMP</li>
+                <li>Phosphate acceptor: 5mM inosine</li>
+                <li>Method: Coupling to recombinant IMP-dehydrogenase (IMPDH, NovoCIB)</li>
+            </ul>
+            <img class="w-100" src="/app/static/img/cNII-phosphotransferase.jpg" alt="Illustration of phosphotransferase activity of cN-II enzyme">
         </div>
         <div class="col-lg-6">
-            <P class="mb-5">
-                <b>Assay condition:</b> 5'-nucleotidase activity of cN-II is followed in an irreversible spectrophotometric assay using coupled purine nucleoside phosphorylase - xanthine dehydrogenase system (2,5 mU/ml each). Assays are carried out at 37'C, at 50mM Tris-HCl pH7,6; 100mM KCl, 20mM MgCl2, 500'M KH2PO4, 5mM DTT, 119 nM cN-II and various concentrations of IMP. Reaction is followed at 295 nm. IMP is purchased from MP Biochemicals. PNP and XDH are produced and purified by NovoCIB.
-            </P>
-            <div class="col-lg-8 mx-auto">
-                <img class="w-100" src="/app/static/img/cN-II-Kinetics.png" alt="cN-II Kinetics">
-            </div>
+            <p><b>Assay condition:</b></p>
+            <ul>
+                <li>Temperature: 37°C</li>
+                <li>Buffer: 50mM Tris-HCl pH7.6</li>
+                <li>Reagents: 100mM KCl, 20mM MgCl2, 500µM KH2PO4, etc.</li>
+                <li>Method: Coupling with purine nucleoside phosphorylase - xanthine dehydrogenase system.</li>
+            </ul>
+            <img class="w-100" src="/app/static/img/cN-II-Kinetics.png" alt="Illustration of cN-II enzyme kinetics">
         </div>
     </div>
 </div>
 
 <div class="container">
     <div class="referances my-5">
-        <h5 class="ms-5 pt-4 pb-3 novo-blue">Scientific Works citing <span class="text-secondary">NOVOCIB</span> Adenosine Kinase and PRECICE® ADK Assay kits:</h5>
+        <h5 class="ms-5 pt-4 pb-3 novo-blue">References (with links to PubMed)</h5>
         <ol>
             <li>
                 <a target="_blank" href="http://www.ncbi.nlm.nih.gov/pubmed/12947102">V. Bianchi and J. Spychala (2003): Mammalian 5'-nucleotidases. <em>J. Biol. Chem.</em> 278(47), 46195-8.</a>
