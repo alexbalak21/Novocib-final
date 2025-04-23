6785027d5bb6f98bac4ea5c493078e6be906fc10
Alex Balak <alex.balak@outlook.com>
2025-04-23 11:14:28 +0200

    update on cn-ii

--- app/views/active-purified-enzymes/cn-ii.php
+++ app/views/active-purified-enzymes/cn-ii.php
@@ -1,17 +1,18 @@
 <?php
 $title = "Human cytosolic 5'-nucleotidase II";
-require_once "app/templates/base.php";
 
-$novoblue = "#4167b1";
-
-
-
-
-addContent(Banner::gen());
-$content_title = UnderlinedTitle::get("Human cytosolic 5'-nucleotidase II (cN-II) (E.C.3.1.3.5)", "novoblue", "center");
 ob_start(); ?>
+<meta name="keywords" content="buy 5'-nucleotidase, recombinant enzyme, cytosolic 5'-nucleotidase, E.C. 3.1.3.5, phosphotransferase, high Km 5'-nucleotidase, HKM-NT, cytosolic purine 5'-nucleotidase, purine 5'-NT, IMP/GMP-specific 5'-nucleotidase, ribavirin phosphorylation">
+<meta name="description" content="Novocib's cloned human cytosolic 5'-nucleotidase II is an active, purified enzyme characterized by its capacity to dephosphorylate GMP and IMP, as well as phosphorylate natural nucleosides like inosine and the antiviral nucleoside analog ribavirin.">
+<?php $metas = ob_get_clean();
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/app/templates/new_base.php";
+?>
+
+<?= Banner::gen() ?>
+
 <div class="container mt-4">
-    <?= $content_title ?>
+    <h2 class="underlinedTitle center"><span class="underlined novoblue center">Human cytosolic 5'-nucleotidase II (cN-II) (E.C.3.1.3.5)</span></h2>
     <div class="col-lg-10 col-12">
         <table class="table product mb-2">
             <thead>
@@ -70,7 +71,7 @@ ob_start(); ?>
         <div class="col-lg-6">
             <p><b>Assay condition:</b> Phosphotransferase activity of human recombinant cN-II was followed at 37°C, in 100mM Tris-HCl pH 9; 100mM KCl, 20mM MgCl2, 0.5mM KH2PO4, 5mM DTT, 5mM NAD, 50mU/mL IMPDH (NovoCIB). 5mM GMP was used as phosphate donor and 5mM inosine as - phosphate acceptor. The formation of IMP from inosine was followed at 340nm through coupling to recombinant IMP-dehydrogenase (IMPDH, NovoCIB). In the presence of NAD, IMPDH catalyzes irreversible conversion of IMP to XMP with concomitant reduction of NAD to NADH2 (specific absorbance at 340nm).</p>
             <div class="col-lg-8 mx-auto">
-                <img class="w-100" src="/app/static/img/cNII-phosphotransferase.jpg" alt="cNII Phosphotransferase">
+                <img class="w-100" src="/app/static/img/cNII-phosphotransferase.jpg" alt="Illustration of phosphotransferase activity of cN-II enzyme">
             </div>
         </div>
         <div class="col-lg-6">
@@ -82,9 +83,33 @@ ob_start(); ?>
             </div>
         </div>
     </div>
-
 </div>
-<?php $page_content = ob_get_clean();
 
-addContent($page_content);
-render();
+<div class="container">
+    <div class="referances my-5">
+        <h5 class="ms-5 pt-4 pb-3 novo-blue">Scientific Works citing <span class="text-secondary">NOVOCIB</span> Adenosine Kinase and PRECICE® ADK Assay kits:</h5>
+        <ol>
+            <li>
+                <a target="_blank" href="http://www.ncbi.nlm.nih.gov/pubmed/12947102">V. Bianchi and J. Spychala (2003): Mammalian 5'-nucleotidases. <em>J. Biol. Chem.</em> 278(47), 46195-8.</a>
+            </li>
+            <li>
+                <a target="_blank" href="http://www.ncbi.nlm.nih.gov/pubmed/9371705">S. Allegrini <em>et al.</em> (1997): Bovine cytosolic IMP/GMP-specific 5'-nucleotidase: cloning and expression of active enzyme in <em>Escherichia coli</em>. <em>Biochem. J.</em> 328(Pt2), 483-487.</a>
+            </li>
+            <li>
+                <a target="_blank" href="http://www.ncbi.nlm.nih.gov/pubmed/18404470">P. L. Ipata and M. G. Tozzi (2006): Recent advances in structure and function of cytosolic IMP-GMP specific 5'-nucleotidase II (cN-II). <em>Purinergic Signal.</em> 2(4), 669-675.</a>
+            </li>
+            <li>
+                <a target="_blank" href="http://www.ncbi.nlm.nih.gov/pubmed/2848805">J. Spychala <em>et al.</em> (1988): High Km Soluble 5'-nucleotidase from human placenta. <em>J. Biol. Chem.</em> 263(35), 18759-65.</a>
+            </li>
+            <li>
+                <a target="_blank" href="http://www.ncbi.nlm.nih.gov/pubmed/2549385">M. A. Johnson and A. Fridland (1989): Phosphorylation of 2',3'-dideoxyinosine by cytosolic 5'-nucleotidase of human lymphoid cells. <em>Mol. Pharmacol.</em> 36(2), 291-295.</a>
+            </li>
+            <li>
+                <a target="_blank" href="http://www.ncbi.nlm.nih.gov/pubmed/2991214">P. M. Keller (1985): Cytoplasmic 5'-nucleotidase catalyzes acyclovir phosphorylation. <em>J. Biol. Chem.</em> 260(15), 8664-7.</a>
+            </li>
+            <li>
+                <a target="_blank" href="http://www.ncbi.nlm.nih.gov/pubmed/15917509">J. Z. Wu <em>et al.</em> (2005): Phosphorylation of ribavirin and viramidine by adenosine kinase and cytosolic 5'-nucleotidase II: Implications for ribavirin metabolism in erythrocytes. <em>Antimicrob. Agents Chemother.</em> 49(6), 2164-71.</a>
+            </li>
+        </ol>
+    </div>
+</div>
